9b669be0f2005305f8ae7ddacbcf6fd19730fa6f
Winston <Winston@WINS-PC>
2017-03-22 18:24:42 -0400

    PRIMERA PRUEBA CON SEGURIDAD OAUTH2

--- src/main/java/bo/afcoop/rec/RecApplication.java
+++ src/main/java/bo/afcoop/rec/RecApplication.java
@@ -2,9 +2,17 @@ package bo.afcoop.rec;
 
 import javax.servlet.Filter;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -27,7 +35,7 @@ public class RecApplication {
 //        config.addAllowedMethod("*");
 //        source.registerCorsConfiguration("/**", config);
 //        return new CorsFilter(source);
-        
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
@@ -40,6 +48,33 @@ public class RecApplication {
         config.addAllowedMethod("DELETE");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
-        
+
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory().withClient("foo").secret("foosecret")
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "password").scopes("openid");
+        }
+
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .csrf().disable()
+                    .anonymous().disable()
+                    .authorizeRequests()
+                    .antMatchers("/oauth/token").permitAll();
+        }
     }
 }
